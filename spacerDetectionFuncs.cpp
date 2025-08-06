752f5421fd4857be177849e4c49597e6fb330aaa
Shachar-katz <sarahkatz45@gmail.com>
2025-08-06 14:59:53 -0700

    fixed bug in spacer table

--- spacerDetectionFuncs.cpp
+++ spacerDetectionFuncs.cpp
@@ -10,7 +10,7 @@ void spacerScraping(const unordered_map<string,Array>& globalArrayMap, unordered
     for (const auto& [arrayId, array] : globalArrayMap){
         vector<string> arrayVect = array.getArrayVect();
         for(int i = 0; i < arrayVect.size(); i++){
-            if (i % 2 == 0){
+            if (i % 2 != 0){
                 string currSpacer = arrayVect.at(i);
                 auto& spacerData = globalSpacerMap[currSpacer];
                 if (spacerData.abundance == 0)
